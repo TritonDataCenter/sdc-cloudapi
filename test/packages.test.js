40d7712a11d67f65b97f94f52617438cfe2f0c19
Pedro P. Candel <pedro@joyent.com>
2012-10-16 08:46:33 +0200

    PUBAPI-421: Differences between 6.5 & 7.0 packages and 7.0 additions.

--- test/packages.test.js
+++ test/packages.test.js
@@ -9,23 +9,38 @@ var common = require('./common');
 
 ///--- Globals
 
-var client, server;
+var client, server, THE_PACKAGE;
 
 
 
 ///--- Helpers
 
-function checkPackage(t, pkg) {
-    t.ok(pkg);
-    t.ok(pkg.name);
-    t.ok(pkg.urn);
-    t.ok(pkg.memory);
-    t.ok(pkg.disk);
-    t.ok(pkg.vcpus);
-    t.ok(pkg.swap);
-    t.ok(pkg['default'] !== undefined);
+function checkPackage_6_5(t, pkg) {
+    t.ok(pkg, 'Package OK');
+    t.ok(pkg.name, 'Package name');
+    t.notOk(pkg.urn, 'Package URN not OK');
+    t.notOk(pkg.uuid, 'Package UUID not OK');
+    t.ok(pkg.memory, 'Package memory');
+    t.ok(pkg.disk, 'Package Disk');
+    t.notOk(pkg.vcpus, 'Package VCPUs not OK');
+    t.notOk(pkg.version, 'Package version not OK');
+    t.ok(pkg.swap, 'Package swap');
+    t.ok(pkg['default'] !== undefined, 'Package default');
 }
 
+
+function checkPackage_7(t, pkg) {
+    t.ok(pkg, 'Package OK');
+    t.ok(pkg.name, 'Package name');
+    t.ok(pkg.urn, 'Package URN OK');
+    t.ok(pkg.uuid, 'Package UUID OK');
+    t.ok(pkg.memory, 'Package memory');
+    t.ok(pkg.disk, 'Package Disk');
+    t.ok(pkg.vcpus, 'Package VCPUs OK');
+    t.ok(pkg.version, 'Package version OK');
+    t.ok(pkg.swap, 'Package swap');
+    t.ok(pkg['default'] !== undefined, 'Package default');
+}
 
 
 ///--- Tests
@@ -44,8 +59,13 @@ test('setup', function (t) {
 });
 
 
-test('ListPackages OK', function (t) {
-    client.get('/my/packages', function (err, req, res, body) {
+test('ListPackages OK (6.5)', function (t) {
+    client.get({
+        path: '/my/packages',
+        headers: {
+            'accept-version': '~6.5'
+        }
+    }, function (err, req, res, body) {
         t.ifError(err);
         t.equal(res.statusCode, 200);
         common.checkHeaders(t, res.headers);
@@ -53,20 +73,99 @@ test('ListPackages OK', function (t) {
         t.ok(Array.isArray(body));
         t.ok(body.length);
         body.forEach(function (p) {
-            checkPackage(t, p);
+            checkPackage_6_5(t, p);
         });
         t.end();
     });
 });
 
 
-test('GetPackage OK', function (t) {
-    client.get('/my/packages/sdc_128', function (err, req, res, body) {
+test('GetPackage OK (6.5)', function (t) {
+    client.get({
+        path: '/my/packages/sdc_128',
+        headers: {
+            'accept-version': '~6.5'
+        }
+    }, function (err, req, res, body) {
         t.ifError(err);
         t.ok(body);
         t.equal(res.statusCode, 200);
         common.checkHeaders(t, res.headers);
-        checkPackage(t, body);
+        checkPackage_6_5(t, body);
+        t.end();
+    });
+});
+
+
+test('ListPackages OK (7.0)', function (t) {
+    client.get({
+        path: '/my/packages',
+        headers: {
+            'accept-version': '~7.0'
+        }
+    }, function (err, req, res, body) {
+        t.ifError(err);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        t.ok(body);
+        t.ok(Array.isArray(body));
+        t.ok(body.length);
+        body.forEach(function (p) {
+            checkPackage_7(t, p);
+        });
+        t.end();
+    });
+});
+
+
+test('GetPackage by name OK (7.0)', function (t) {
+    client.get({
+        path: '/my/packages/sdc_128',
+        headers: {
+            'accept-version': '~7.0'
+        }
+    }, function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        checkPackage_7(t, body);
+        THE_PACKAGE = body;
+        console.log(util.inspect(THE_PACKAGE, false, 8));
+        t.end();
+    });
+});
+
+
+test('GetPackage by UUID OK (7.0)', function (t) {
+    client.get({
+        path: '/my/packages/' + THE_PACKAGE.uuid,
+        headers: {
+            'accept-version': '~7.0'
+        }
+    }, function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        checkPackage_7(t, body);
+        t.end();
+    });
+});
+
+
+test('GetPackage by URN OK (7.0)', function (t) {
+    client.get({
+        path: '/my/packages/' + THE_PACKAGE.urn.replace(/:/g, "%3A"),
+        headers: {
+            'accept-version': '~7.0'
+        }
+    }, function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        checkPackage_7(t, body);
         t.end();
     });
 });
