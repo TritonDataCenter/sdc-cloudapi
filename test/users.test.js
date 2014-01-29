29c0fa3acb6d04fea8e8b2f4e39fc3758d03d7fc
Pedro P. Candel <pedro@joyent.com>
2014-01-29 16:49:01 +0100

    PUBAPI-795: Account roles

--- test/users.test.js
+++ test/users.test.js
@@ -9,12 +9,34 @@ var common = require('./common');
 
 // --- Globals
 
+var USER_FMT = 'uuid=%s, ou=users, o=smartdc';
+var SUB_FMT = 'uuid=%s, ' + USER_FMT;
+var ROLE_FMT = 'role-uuid=%s, ' + USER_FMT;
+
 var client, server, account;
 var SUB_ID = libuuid.create();
 var SUB_LOGIN = 'a' + SUB_ID.substr(0, 7);
 var SUB_EMAIL = SUB_LOGIN + '_test@joyent.com';
 var SUB_UUID;
+var SUB_DN;
 var PWD = 'joypass123';
+
+var SUB_ID_TWO = libuuid.create();
+var SUB_LOGIN_TWO = 'a' + SUB_ID_TWO.substr(0, 7);
+var SUB_EMAIL_TWO = SUB_LOGIN_TWO + '_test@joyent.com';
+var SUB_UUID_TWO;
+var SUB_DN_TWO;
+
+var POLICY_DOC = [
+    'Fred can read *.js when dirname = examples and sourceip = 10.0.0.0/8',
+    'Bob can read and write timesheet if requesttime::time > 07:30:00 and ' +
+        'requesttime::time < 18:30:00 and ' +
+        'requesttime::day in (Mon, Tue, Wed, THu, Fri)',
+    'John, Jack and Jane can ops_* *'
+];
+
+var ROLE_UUID;
+var ROLE_DN;
 
 // --- Helpers
 function checkUser(t, user) {
@@ -22,6 +44,13 @@ function checkUser(t, user) {
     t.ok(user.id, 'checkUser user.id OK');
     t.ok(user.login, 'checkUser user.login OK');
     t.ok(user.email, 'checkUser user.email OK');
+}
+
+function checkRole(t, role) {
+    t.ok(role, 'checkRole role OK');
+    t.ok(role.id, 'checkRole role.id OK');
+    t.ok(role.name, 'checkRole role.name OK');
+    t.ok(role.policy, 'checkRole role.policy OK');
 }
 
 // --- Tests
@@ -114,6 +143,7 @@ test('create user', function (t) {
         common.checkHeaders(t, res.headers);
         checkUser(t, body);
         SUB_UUID = body.id;
+        SUB_DN = util.format(SUB_FMT, SUB_UUID, account.uuid);
         t.end();
     });
 });
@@ -171,8 +201,132 @@ test('list users OK', function (t) {
 });
 
 
+test('create another user', function (t) {
+    var user = {
+        login: SUB_LOGIN_TWO,
+        email: SUB_EMAIL_TWO,
+        password: PWD
+    };
+
+    client.post('/my/users', user, function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+        t.equal(res.statusCode, 201);
+        common.checkHeaders(t, res.headers);
+        checkUser(t, body);
+        SUB_UUID_TWO = body.id;
+        SUB_DN_TWO = util.format(SUB_FMT, SUB_UUID_TWO, account.uuid);
+        t.end();
+    });
+});
+
+
+test('list roles (empty)', function (t) {
+    client.get('/my/roles', function (err, req, res, body) {
+        t.ifError(err);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        t.ok(body);
+        t.ok(Array.isArray(body));
+        t.equal(body.length, 0);
+        t.end();
+    });
+});
+
+
+test('create role', function (t) {
+    var role_uuid = libuuid.create();
+    var name = 'a' + role_uuid.substr(0, 7);
+
+    var entry = {
+        name: name,
+        policy: POLICY_DOC,
+        members: SUB_DN,
+        description: 'This is completely optional'
+    };
+
+    client.post('/my/roles', entry, function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+        t.equal(res.statusCode, 201);
+        common.checkHeaders(t, res.headers);
+        checkRole(t, body);
+        ROLE_UUID = body.id;
+        ROLE_DN = util.format(ROLE_FMT, ROLE_UUID, account.uuid);
+        t.end();
+    });
+});
+
+
+test('get role by UUID', function (t) {
+    client.get('/my/roles/' + ROLE_UUID, function (err, req, res, body) {
+        t.ifError(err);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        t.ok(body);
+        t.equal(body.id, ROLE_UUID);
+        t.end();
+    });
+});
+
+
+test('list roles (OK)', function (t) {
+    client.get('/my/roles', function (err, req, res, body) {
+        t.ifError(err);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        t.ok(body);
+        t.ok(Array.isArray(body));
+        t.equal(body.length, 1);
+        t.end();
+    });
+});
+
+
+test('update role', function (t) {
+    var str = 'Pedro can delete *';
+    POLICY_DOC.push(str);
+    client.post('/my/roles/' + ROLE_UUID, {
+        policy: POLICY_DOC,
+        name: 'role-name-can-be-modified'
+    }, function (err, req, res, body) {
+        t.ifError(err);
+        t.ok(body);
+        t.equal(res.statusCode, 200);
+        common.checkHeaders(t, res.headers);
+        checkRole(t, body);
+        t.equal(body.name, 'role-name-can-be-modified');
+        t.ok(body.policy.indexOf(str) !== -1);
+        t.end();
+    });
+});
+
+
+test('delete role', function (t) {
+    var url = '/my/roles/' + ROLE_UUID;
+    client.del(url, function (err, req, res) {
+        t.ifError(err);
+        t.equal(res.statusCode, 204);
+        common.checkHeaders(t, res.headers);
+        t.end();
+    });
+});
+
+
+
 test('delete user', function (t) {
     var url = '/my/users/' + SUB_LOGIN;
+    client.del(url, function (err, req, res) {
+        t.ifError(err);
+        t.equal(res.statusCode, 204);
+        common.checkHeaders(t, res.headers);
+        t.end();
+    });
+});
+
+
+test('delete another user', function (t) {
+    var url = '/my/users/' + SUB_LOGIN_TWO;
     client.del(url, function (err, req, res) {
         t.ifError(err);
         t.equal(res.statusCode, 204);
