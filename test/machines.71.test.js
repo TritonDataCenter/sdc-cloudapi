1e247a774a19d3310ea513312209b294ecffc31b
Pedro P. Candel <pedro@joyent.com>
2014-04-11 09:42:29 +0200

    PUBAPI-863: Machines tests always picking first dataset with independence of version.

--- test/machines.71.test.js
+++ test/machines.71.test.js
@@ -148,7 +148,7 @@ test('get base dataset', TAP_CONF, function (t) {
         t.ok(body.length, 'GET /my/datasets body array has elements');
         body.forEach(function (d) {
             if (d.version && d.version === '13.4.0') {
-                DATASET = body[0].id;
+                DATASET = body[d].id;
             }
         });
         t.end();
